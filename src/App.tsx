46a9527ec1451e2ccdbdb8d122f90a1f2b257877
warabimochi <ecccompsenyou@gmail.com>
2024-02-24 08:07:40 +0900

    6

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,31 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
+
+const name = "hello";
+
+let username = "Hello";
+let dummyNum = 2;
+let bool = true;
+
+let array1 = [true, false, true];
+let array2 = [0, 1, "hello"];
+
+interface NAME {
+  first: string;
+  last: string | null;
+}
+
+let nameObj: NAME = { first: "Yamada", last: null };
+
+const func1 = (x: number, y: number): number => {
+  return x + y;
+};
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <header className="App-header"></header>
     </div>
   );
 }
