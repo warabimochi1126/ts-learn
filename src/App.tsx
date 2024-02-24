e06ec4651ad28a23d047c7b90ed44e0d39894e31
warabimochi <satomi920003@yahoo.co.jp>
2024-02-25 05:43:01 +0900

    12

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { memo } from "react";
 import logo from "./logo.svg";
 import "./App.css";
 
@@ -11,6 +11,8 @@ let bool = true;
 let array1 = [true, false, true];
 let array2 = [0, 1, "hello"];
 
+let array3: (number | string)[] = [0, 1, 2];
+
 interface NAME {
   first: string;
   last: string | null;
@@ -21,6 +23,92 @@ let nameObj: NAME = { first: "Yamada", last: null };
 const func1 = (x: number, y: number): number => {
   return x + y;
 };
+
+// Intersection Types
+type PROFILE = {
+  age: number;
+  city: string;
+};
+
+type LOGIN = {
+  username: string;
+  password: string;
+};
+
+type USER = PROFILE & LOGIN;
+
+let userA: USER = {
+  age: 30,
+  city: "Tokyo",
+  username: "xxx",
+  password: "yyy"
+}
+
+// Union Types
+let value: boolean | number;
+value = true;
+value =  10;
+
+let arrayUni: (number | string)[];
+arrayUni = [0, 1, 2, "hello"];
+
+// Literal Type
+let company: "Facebook" | "Google" | "Amazon";
+company = "Amazon";
+
+let memory: 256 | 512;
+memory = 512;
+
+// typeof
+let msg: string = "Hi";  
+let msg2: typeof msg;
+msg2 = "hello";
+
+let animal = { cat: "small cat" };
+let newAnimal: typeof animal = { cat: "big cat" };
+
+// keyof
+type KEYS = {
+  primary: string;
+  secondary: string;
+}
+
+let key: keyof KEYS;
+key = "primary";
+
+// typeof + keyof
+const SPORTS = {
+  soccer: "Soccer",
+  baseball: "Baseball"
+};
+
+let keySports: keyof typeof SPORTS;
+keySports = "soccer";
+
+// enum
+enum OS {
+  Windows,
+  Mac,
+  Linux
+}
+
+interface PC {
+  id: number;
+  OSType: OS;
+}
+
+const PC1: PC = {
+  id: 0,
+  OSType: OS.Windows
+}
+
+const PC2: PC = {
+  id: 1,
+  OSType: OS.Mac
+}
+
+
+
 
 function App() {
   return (
